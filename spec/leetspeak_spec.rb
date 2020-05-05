81ee5a17c6e91637b7e6351cf08fb40b0f4c92e1
Ryan Duff <ryanduff613@gmail.com>
2020-05-05 01:11:52 -0700

    write second spec

--- spec/leetspeak_spec.rb
+++ spec/leetspeak_spec.rb
@@ -5,4 +5,8 @@ describe('String#leetspeak') do
   it('returns a string when no Leetspeak applies') do
     expect(leetspeak("happy")).to(eq("happy"))
   end
+
+  it('replaces all e characters with 3') do
+    expect(leetspeak("elephant")).to(eq("3l3phant"))
+  end
 end
