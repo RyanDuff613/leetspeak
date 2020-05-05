6262081acb5d041f6db1eb8a4fbf7ab0e61cb6b9
Ryan Duff <ryanduff613@gmail.com>
2020-05-05 01:33:51 -0700

    add code to convert e characters into 3s, passing tests

--- lib/leetspeak.rb
+++ lib/leetspeak.rb
@@ -2,5 +2,14 @@ require ('pry')
 
 def leetspeak(string)
   userString = string
-  userString
+  userLetters = userString.split("")
+  convertedLetters = []
+  userLetters.each do |letter|
+    if letter != "e"
+      convertedLetters.push(letter)
+    else
+      convertedLetters.push("3")
+    end
+  end
+  convertedLetters.join()
 end
